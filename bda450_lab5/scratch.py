c5f781d2043388213299c9be757f936a7b91cf67
victoriavv4 <90003888+victoriavv4@users.noreply.github.com>
2022-02-21 15:30:02 -0500

    Delete scratch.py

--- bda450_lab5/scratch.py
+++
@@ -1,9 +0,0 @@
-import random
-
-rand_num = random.randint(1, 3)
-print(rand_num)
-
-print(f"The average delay time is across multiple runs is: "
-              f" {sum(self.total_delay_list) / sum(self.number_delayed_list)}")
-        print(f'The standard deviation of delay time across multiple runs is: '
-              f'{statistics.pstdev(self.total_delay_list)}')
